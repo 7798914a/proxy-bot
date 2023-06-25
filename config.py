7b5b8ef9ef4191f9decb30e87a06b979e4ce9ca6
7798914a <128619025+7798914a@users.noreply.github.com>
2023-06-26 00:19:05 +0800

    Update config.py
    4

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ PROXY_FILE = "proxy.txt" #proxy data save file (absolute path)
 POINTS_PER_DAY = 1 #please do not modify
 NEWS = "" # /proxy and /tgproxy bottom message
 
-token = "" # telegram bot token
-adminlist = [114, 514] # admin telegram ID
+token = "6299864243:AAENSCs6lcclFRWjBnlWguCU93infdw5_s0" # telegram bot token
+adminlist = [5411169541] # admin telegram ID
 
 backend = ["https://api.ffq.la/", "https://subscribe.cctv.rip/", "https://api.v1.mk/"] #subscribe convert backend api
 ini = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini" #subscribe convert config

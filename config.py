cbc796e80b5f0453bcb7f6e4983ddff4418881a1
7798914a <128619025+7798914a@users.noreply.github.com>
2023-06-26 01:16:14 +0800

    Update config.py
    aa

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 
-DATA_FILE = "data.json" #user data save file (absolute path)
-TGPROXY_FILE = "tgproxy.txt" #mtproxy data save file (absolute path)
-PROXY_FILE = "proxy.txt" #proxy data save file (absolute path)
-POINTS_PER_DAY = 1 #please do not modify
-NEWS = "" # /proxy and /tgproxy bottom message
+数据文件=" data.json " #用户数据保存文件（绝对路径)
+TGPROXY_FILE =" tgproxy.txt " #mtproxy数据保存文件（绝对路径)
+代理文件=" proxy.txt " #代理数据保存文件（绝对路径)
+每日点数=1 #请不要修改
+新闻="" #/代理和/tgproxy底部消息
 
-token = "" # telegram bot token
-adminlist = [114, 514] # admin telegram ID
+令牌=" 6299864243:aaenscs 6 lclfrwjbnlwgucu 93 info dw 5 _ s0 " #电报机器人令牌
+管理列表=[5411169541] #管理电报身份证明
 
-backend = ["https://api.ffq.la/", "https://subscribe.cctv.rip/", "https://api.v1.mk/"] #subscribe convert backend api
-ini = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini" #subscribe convert config
+后端=[“https://api.ffq.la/", “https://subscribe.cctv.rip/", “https://api.v1.mk/"] #订阅转换后端美国石油学会(American Petroleum Institute)
+ini =" https://raw。githubusercontent。com/ACL 4 SRS/ACL 4 SRS/master/Clash/config/ACL 4 SRS _ online。ini " #订阅转换配置
